c2c2feb95110d99323be6e4a2aa2569407a5cbfb
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-26 15:40:16 +0400

    add material ui

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,9 +1,13 @@
 import { Route, Routes } from "react-router";
-
+import { Button } from "@mui/material"
 function WebRouting() {
     return (
         <Routes>
-            <Route path="/" element={<div>I'm Home page</div>} />
+            <Route path="/" element={<div>
+                I'm Home page
+                <Button variant="contained">click me</Button>
+
+            </div>} />
             <Route path="/about" element={<div>I'm about page</div>} />
         </Routes>
     )

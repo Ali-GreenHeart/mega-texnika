2c0efbe086a584becccfc3916f3e45929df8579e
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-29 14:45:54 +0400

    add css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import CssBaseline from '@mui/material/CssBaseline'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
     <BrowserRouter>
       <App />
     </BrowserRouter>

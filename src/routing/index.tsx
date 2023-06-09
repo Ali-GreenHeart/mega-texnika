92098f8dc958854df9994311850af565b8bc4293
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-06-09 15:43:17 +0400

    add blog page to routing

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import HomePage from "../pages/Home";
 import Texnikalar from "../pages/Texnikalar";
+import BlogPage from "../pages/Blog";
 
 function WebRouting() {
   return (
@@ -8,6 +9,7 @@ function WebRouting() {
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<div>I'm about page</div>} />
       <Route path="/texnikalar" element={<Texnikalar />} />
+      <Route path="/blog" element={<BlogPage />} />
     </Routes>
   );
 }
